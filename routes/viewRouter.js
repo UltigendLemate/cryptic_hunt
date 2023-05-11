7d63fa363ce44898da7b4a3d1ac954338a14c65b
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-05-12 00:39:22 +0530

    roundtable path

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -5,48 +5,54 @@ const playController = require("../controllers/playController");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-	res.status(200).render("login", {
-		title: "Login",
-	});
+    res.status(200).render("login", {
+        title: "Login",
+    });
 });
 
 router.get("/signupAdmin", (req, res) => {
-	res.status(200).render("signup", {
-		title: "Sign Up",
-	});
+    res.status(200).render("signup", {
+        title: "Sign Up",
+    });
 });
 
 // Just to check if user is logged in or not so that the header can render accordingly
 router.use(userController.isLoggedIn);
 
 router.get("/leaderboard", async (req, res) => {
-	const users = await playController.getDashboardView();
-	res.status(200).render("leaderboard", {
-		title: "Dashboard",
-		users,
-	});
+    const users = await playController.getDashboardView();
+    res.status(200).render("leaderboard", {
+        title: "Dashboard",
+        users,
+    });
+});
+
+router.get("/roundtable", (req, res) => {
+    res.status(200).render("index", {
+        title: "ABC",
+    });
 });
 
 router.get("/", (req, res) => {
-	res.status(200).render("index", {
-		title: "Home",
-	});
+    res.status(200).render("index", {
+        title: "Home",
+    });
 });
 
 router.use(userController.protect);
 
 router.get("/play", async (req, res) => {
-	const question = await playController.getViewQuestion(req);
-	res.status(200).render("play", {
-		question,
-		title: "Play",
-	});
+    const question = await playController.getViewQuestion(req);
+    res.status(200).render("play", {
+        question,
+        title: "Play",
+    });
 });
 
 router.get("/newQuestion", async (req, res) => {
-	res.status(200).render("addQuestion", {
-		title: "New Question",
-	});
+    res.status(200).render("addQuestion", {
+        title: "New Question",
+    });
 });
 
 module.exports = router;
