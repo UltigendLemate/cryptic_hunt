f8e502fe6f41345c9c1907068586391cfe7ef40d
t-vaibhav <118555370+t-vaibhav@users.noreply.github.com>
2023-05-13 10:52:44 +0530

    deplyoment issues

--- public/js/base.js
+++ public/js/base.js
@@ -1,8 +1,4 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.133.1/build/three.module";
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls';
-import { EffectComposer } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/RenderPass';
-import { UnrealBloomPass } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/UnrealBloomPass';
+
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
